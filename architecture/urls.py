00790cbf51464326cd257b0082b8a8d24a893897
cabellohuaranga <cabello.huaranga.unas@gmail.com>
2021-09-10 09:33:52 -0500

    first commit model, serializer, views and urls

--- architecture/urls.py
+++ architecture/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from library.views import StudentList, StudentDetail
+from library import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/books/list', StudentList.as_view()),
-    path('api/v1/books/details/<int:pk>', StudentDetail.as_view())
+    path('api/v1/student/list', views.ListStudentAPIView.as_view(), name="student_list"),
+    path('api/v1/student/create', views.CreateStudentAPIView.as_view(), name="student_create"),
+    path('api/v1/student/update/<int:pk>', views.UpdateStudentAPIView.as_view(), name="student_update"),
+    path('api/v1/student/delete/<int:pk>', views.DeleteStudentAPIView.as_view(), name="student_delete"),
 ]
--- library/views.py
+++ library/views.py
@@ -1,15 +1,33 @@
-from django.shortcuts import render
-from rest_framework import generics
+from rest_framework.generics import ListAPIView
+from rest_framework.generics import CreateAPIView
+from rest_framework.generics import DestroyAPIView
+from rest_framework.generics import UpdateAPIView
+
 from library.models import Student
+from django.shortcuts import render
 from library.serializer import StudentSerializer
 
 
 # Create your views here.
-class StudentList(generics.ListCreateAPIView):
+class ListStudentAPIView(ListAPIView):
+    """" List all """
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
 
-class StudentDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Student
+class CreateStudentAPIView(CreateAPIView):
+    """ Creations """
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+
+class UpdateStudentAPIView(UpdateAPIView):
+    """ Updating """
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+
+class DeleteStudentAPIView(DestroyAPIView):
+    """ Deletion """
+    queryset = Student.objects.all()
     serializer_class = StudentSerializer
